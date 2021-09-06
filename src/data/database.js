1060577f3ef247baf9da2b45c7d7e6c666bdc927
juliathemelo <juliarexsteam@hotmail.com>
2021-09-06 15:41:38 -0300

    db nas nuvens config

--- src/data/database.js
+++ src/data/database.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose')
 require('dotenv').config()
 
+const MONGO_URL = process.env.MONGODB_URI
+
 const connect = () => {
-    mongoose.connect(process.env.DATABASE_URL , {
+    mongoose.connect(MONGO_URL , {
         useNewUrlParser: true,
         useUnifiedTopology: true
     }).then(console.log('Database connect')
